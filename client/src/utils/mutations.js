e96db20aab6fa7c3040dac892a616665293b2ea7
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-14 21:28:04 -0700

    added new components and files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,24 @@
+import gql from "graphql-tag";
+
+// mutation for logged in user
+export const LOGIN_USER = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          link
+          image
+        }
+      }
+    }
+  }
+`;
