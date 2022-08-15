e1d5b5f6bf76ffddcdc860867e16073da3221bf5
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-14 20:16:44 -0700

    updated auth in utilities folder

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-const secret = "mysecretsshhhhh";
+// set token secret and expiration date
+const secret = "notsosecret";
 const expiration = "2h";
 
 module.exports = {
+  // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via req.body, req.query, or headers
+    // allows token to be sent via  req.query or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -17,6 +19,7 @@ module.exports = {
       return req;
     }
 
+    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -24,6 +27,7 @@ module.exports = {
       console.log("Invalid token");
     }
 
+    // send to next endpoint
     return req;
   },
   signToken: function ({ username, email, _id }) {
