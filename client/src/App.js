7632ba6dca1affe0b79eb067e519c50de26514f4
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-15 19:39:19 -0700

    Commented out Appollo stuff for now

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,27 @@ import Header from "../src/components/Header";
 import About from "./components/Landing";
 // import { Helmet } from "react-helmet";
 import Footer from "../src/components/Footer";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
+// import { ApolloProvider } from "@apollo/react-hooks";
+// import ApolloClient from "apollo-boost";
 
 // import logo from "./logo.svg";
 import "./App.css";
 
 //establish apollo client
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
+// const client = new ApolloClient({
+//   request: (operation) => {
+//     const token = localStorage.getItem("id_token");
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-  uri: "/graphql",
-});
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : "",
+//       },
+//     });
+//   },
+//   uri: "/graphql",
+// });
 
 function App() {
   const [currentTab, handleTabChange] = useState("about");
@@ -46,25 +46,25 @@ function App() {
   };
 
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Header
-            currentTab={currentTab}
-            handleTabChange={handleTabChange}
-          ></Header>
-          <main>{renderTab()}</main>
+    // <ApolloProvider client={client}>
+    //   <Router>
+    <>
+      <Header
+        currentTab={currentTab}
+        handleTabChange={handleTabChange}
+      ></Header>
+      <main>{renderTab()}</main>
 
-          {/* <Switch>
+      {/* <Switch>
             <Route exact path="/" component={SearchBooks} />
             <Route exact path="/saved" component={SavedBooks} />
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch> */}
 
-          <Footer></Footer>
-        </>
-      </Router>
-    </ApolloProvider>
+      <Footer></Footer>
+    </>
+    //   </Router>
+    // </ApolloProvider>
   );
 }
 
