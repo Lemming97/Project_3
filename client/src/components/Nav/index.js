c451d87557bdb016938e103014c50d0843268614
Scott Moore <sm91srjc@gmail.com>
2022-08-16 21:00:02 -0700

    stripe working as well as snackbar notifcation, check NAV index.js line 84 for card payment testing information

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
+import StripeCheckout from "react-stripe-checkout";
+import { useSnackbar } from "notistack";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -10,6 +12,11 @@ function Nav(props) {
     document.title = capitalizeFirstLetter(currentTab.name);
   }, [currentTab]);
 
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  const paymentSubmit = () => {
+    enqueueSnackbar("The Sphere thanks you!");
+  };
   return (
     <nav>
       <ul className="nav nav-tabs">
@@ -73,6 +80,17 @@ function Nav(props) {
             Login/Signup
           </a>
         </li>
+        <StripeCheckout
+          // When testing strip use 42 repeated as the cardnumber 123 as the date and any three numbers for the CVC code
+          stripeKey="pk_test_51LXcTCI4xTgSuEekRHNVfZ7PdNqf4Zfz2DzqHonYQzLoCQFcjp4gdai2bi27kGivKzzvyUQujpmdORmHTB0ZZLKO00iHSL8Vf0"
+          name="Give the Sphere your money"
+          panelLabel="Quick click me!"
+          token={paymentSubmit}
+        >
+          <li className="nav-item">
+            <a className="nav-link">Donate</a>
+          </li>
+        </StripeCheckout>
       </ul>
     </nav>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
+import { SnackbarProvider } from "notistack";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider
+      variant="success"
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "center",
+      }}
+    >
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>
 );
 
