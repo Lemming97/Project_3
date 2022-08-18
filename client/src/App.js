67fbd1d78cf26c46a0a1a8172eb2b7c0fad7819a
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-17 21:31:33 -0700

    Commented out switch case and used basic routes structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //set apollo.router and client
 import {
@@ -12,7 +13,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 //pages
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./pages/LandingPage";
 import "./App.css";
 
@@ -52,21 +53,34 @@ const client = new ApolloClient({
 });
 
 function App() {
+  //using swith chase which seems to be breaking at the moment
+  // <ApolloProvider client={client}>
+  //   <Router>
+  //     <>
+  //       <Navbar />
+  //       <Switch>
+  //         <Route exact path="/" component={LandingPage} />
+  //         {/* <Route exact path="/saved" component={SavedBooks} /> */}
+  //         <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+  //       </Switch>
+  //     </>
+  //   </Router>
+  // </ApolloProvider>;
+  // return <LandingPage />;
+
   <ApolloProvider client={client}>
     <Router>
-      <>
-        <Navbar />
-
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          {/* <Route exact path="/saved" component={SavedBooks} /> */}
-          <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-        </Switch>
-      </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/thought/:id" element={<SingleThought />} />
+        <Route path="*" element={<NoMatch />} /> */}
+      </Routes>
     </Router>
   </ApolloProvider>;
-
-  // return <LandingPage />;
+  return <LandingPage />;
 }
 
 export default App;
