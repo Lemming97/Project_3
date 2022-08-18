e87dc0f6a489fd2b07928797e9446c3e69528215
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-17 20:23:24 -0700

    fixed merge conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// import reportWebVitals from './reportWebVitals';
-import { SnackbarProvider } from "notistack";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SnackbarProvider
-      variant="success"
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "center",
-      }}
-    >
-      <App />
-    </SnackbarProvider>
+    <App />
   </React.StrictMode>
 );
