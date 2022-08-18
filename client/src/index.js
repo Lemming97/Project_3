24c7f37b8729c49735c99d0667161d9226b5772b
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-17 20:23:13 -0700

    fixed merge conflicts

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-<<<<<<< HEAD
+
 // import reportWebVitals from './reportWebVitals';
 import { SnackbarProvider } from "notistack";
-=======
->>>>>>> develop
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
