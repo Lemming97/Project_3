45cbbf3c199b6f2cd580d54dc149efba64b77a67
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-22 10:25:17 -0700

    commented out the main model line and just included what is currently there

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,15 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User, SorcerersSphere, MagicMark, LightWorker, Sphericle } = require("../models");
+// const { User, SorcerersSphere, MagicMark, LightWorker, Sphericle } = require("../models");
+const { User, Sphericle } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
-          .select("-__v -password");
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
 
         return userData;
       }
@@ -15,29 +17,26 @@ const resolvers = {
       throw new AuthenticationError("Not logged in");
     },
     getAllUsers: async () => {
-      const userData = await User.find({})
-        .select('-__v -password');
+      const userData = await User.find({}).select("-__v -password");
 
       return userData;
     },
     getSingleSphericle: async (parent, { _id }, context) => {
       if (context.user) {
-        const data = await Sphericle.findOne({ _id: _id })
-          .select('-__v');
+        const data = await Sphericle.findOne({ _id: _id }).select("-__v");
       }
 
       throw new AuthenticationError("Not logged in");
     },
     getByFacet: async (parent, { facet }, context) => {
       if (context.user) {
-        const facetData = await Sphericle.find({ facet: facet })
-          .select('-__v');
+        const facetData = await Sphericle.find({ facet: facet }).select("-__v");
 
         return facetData;
       }
 
       throw new AuthenticationError("Not logged in");
-    }
+    },
   },
 
   Mutation: {
@@ -66,27 +65,27 @@ const resolvers = {
     upvote: async (parent, { _id, voteCount }, context) => {
       if (context.user) {
         const updatedSphericle = await Sphericle.findOneandUpdate(
-          { _id: _id }, 
+          { _id: _id },
           // this may not be the way to augment. Create separate function const?
           { voteCount: voteCount++ },
           { new: true }
-        )
+        );
         return updatedSphericle;
       }
 
-      throw new AuthenticationError("Not logged in")
+      throw new AuthenticationError("Not logged in");
     },
     downvote: async (parent, { _id, voteCount }, context) => {
       if (context.user) {
         const updatedSphericle = await Sphericle.findOneandUpdate(
-          { _id: _id }, 
+          { _id: _id },
           { voteCount: voteCount-- },
           { new: true }
-        )
+        );
         return updatedSphericle;
       }
-      throw new AuthenticationError("Not logged in")
-    }
+      throw new AuthenticationError("Not logged in");
+    },
   },
 };
 
