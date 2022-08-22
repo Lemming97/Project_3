2fdf8ff66e9109951b04b4b16e586449fd855d11
Scott Moore <sm91srjc@gmail.com>
2022-08-21 23:58:24 -0700

    initial 18+ verification created

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -10,6 +10,7 @@ const LandingPage = () => {
       <div className="px-4 sm:px-14">
         <HeroSection />
       </div>
+      <EighteenPlus />
       <DetailsSection />
       <div className="flex justify-center items-center mt-10">
         <img src={image} alt="" className="lg:w-3/6" />
@@ -17,5 +18,27 @@ const LandingPage = () => {
     </>
   );
 };
+
+const EighteenPlus = () => {
+  return (
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-50  flex justify-center items-center">
+      <div className="bg-white p-10 rounded-md space-y-4">
+        <div className="text-2xl">Are you 18 or older?</div>
+        <div className="space-x-4 flex justify-end">
+          <ScottsButton>No</ScottsButton>
+          <ScottsButton>Yes</ScottsButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function ScottsButton({ children }) {
+  return (
+    <button className="bg-darkGrey text-white py-2 px-6 rounded-md">
+      {children}
+    </button>
+  );
+}
 
 export default LandingPage;
