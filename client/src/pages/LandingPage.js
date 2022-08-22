a7418fa0fc3e961676b531befc1d3fd15075b818
Scott Moore <sm91srjc@gmail.com>
2022-08-22 01:37:54 -0700

    button onclicks working

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,16 +1,28 @@
-import React from "react";
 import DetailsSection from "../components/DetailsSection/DetailsSection";
 import HeroSection from "../components/HeroSection/HeroSection";
+import React, { useState } from "react";
 
 import image from "../assets/images/hands.png";
+import { useHref } from "react-router-dom";
 
 const LandingPage = () => {
+  const [shouldShowModal, setShouldShowModal] = useState(true);
+
+  const onYesClick = () => {
+    setShouldShowModal(false);
+  };
+
+  const onNoClick = () => {
+    window.location.href = "https://www.chuckecheese.com/";
+    setShouldShowModal(false);
+  };
+
   return (
     <>
       <div className="px-4 sm:px-14">
         <HeroSection />
       </div>
-      <EighteenPlus />
+      {shouldShowModal && <EighteenPlus yes={onYesClick} no={onNoClick} />}
       <DetailsSection />
       <div className="flex justify-center items-center mt-10">
         <img src={image} alt="" className="lg:w-3/6" />
@@ -19,26 +31,38 @@ const LandingPage = () => {
   );
 };
 
-const EighteenPlus = () => {
+const EighteenPlus = (props) => {
+  console.log("eighteenPlus", props);
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-50  flex justify-center items-center">
-      <div className="bg-white p-10 rounded-md space-y-4">
+      <div className="bg-white p-8 rounded-md space-y-4 max-w-md">
         <div className="text-2xl">Are you 18 or older?</div>
+        <p>
+          If you are not 18 dont click yes or Mark is going to find out and you
+          dont want Mark to find out
+        </p>
         <div className="space-x-4 flex justify-end">
-          <ScottsButton>No</ScottsButton>
-          <ScottsButton>Yes</ScottsButton>
+          <ScottsButton onClick={props.no}>No</ScottsButton>
+          <ScottsButton onClick={props.yes}>Yes</ScottsButton>
         </div>
       </div>
     </div>
   );
 };
 
-function ScottsButton({ children }) {
+function ScottsButton(props) {
+  console.log("scottsButton", props);
   return (
-    <button className="bg-darkGrey text-white py-2 px-6 rounded-md">
-      {children}
+    <button
+      onClick={props.onClick}
+      className="bg-darkGrey text-white py-2 px-6 rounded-md"
+    >
+      {props.children}
     </button>
   );
 }
 
 export default LandingPage;
+
+// on click yes it returns a do not render eifghteenplus
+// on click yes reroutes to google
