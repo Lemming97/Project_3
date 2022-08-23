54498bc30075259f115b5d7d4b900d8f2f78b851
Lindsey Martin <49573760+Lemming97@users.noreply.github.com>
2022-08-22 22:04:11 -0700

    added share icon to magic mark's page

--- client/src/pages/SorcerersSpherePage.js
+++ client/src/pages/SorcerersSpherePage.js
@@ -60,18 +60,7 @@ const SorcerersSpherePage = () => {
               <p className="text-2xl tracking-wider leading-loose">
                 {DUMMY_TEXT}
               </p>
-              {/* <button className="absolute right-6 bottom-6">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="40"
-                  height="40"
-                  fill="#83a2ab"
-                  className="bi bi-share-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
-                </svg>
-              </button> */}
+
               <button
                 className="px-4 py-2 absolute right-6 bottom-6 share-button"
                 type="button"
